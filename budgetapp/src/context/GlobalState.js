6b9f0833da6575c15f72699b00faac47dbec7434
andyrich75 <arichard101@gmail.com>
2020-03-02 16:04:45 -0500

    created the Global State for reducer

--- budgetapp/src/context/GlobalState.js
+++ budgetapp/src/context/GlobalState.js
@@ -1,2 +1,28 @@
 import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
 
+
+//create an initial state 
+const initialState = {
+    transactions: [
+        { id: 1, text: 'Salary', amount: 3000},
+        { id: 2, text: 'Car Payment', amount: -300},
+        { id: 3, text: 'Food', amount: -100},
+        { id: 4, text: 'Check for Birthday', amount: 50},
+    ]
+}
+
+//create Global context to be passed in
+export const GlobalContext = createContext(initialState);
+
+
+//Provider component
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions
+    }}>
+        {children}
+    </GlobalContext.Provider>);
+}
