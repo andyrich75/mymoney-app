0a92745592a53b73a407ba38be04b226d340dd00
andyrich75 <arichard101@gmail.com>
2020-03-02 18:52:35 -0500

    added the port to run on

--- budgetapp/server.js
+++ budgetapp/server.js
@@ -2,3 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+
+dotenv.config({path: './config/config.env'});
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello'));
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
