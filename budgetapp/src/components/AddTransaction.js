1f91303465cc3a810c1e311ceb6c9ae6d265da8c
andyrich75 <arichard101@gmail.com>
2020-03-02 15:30:34 -0500

    Cleandup the id's and ClassNames

--- budgetapp/src/components/AddTransaction.js
+++ budgetapp/src/components/AddTransaction.js
@@ -7,15 +7,15 @@ export const AddTransaction = () => {
             <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
-          <label htmlfor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <label htmlFor="text">Text</label>
+          <input type="text" placeholder="Enter text..." />
         </div>
         <div className="form-control">
-          <label for="amount"
+          <label htmlFor="amount"
             >Amount <br />
             (negative - expense, positive - income)</label
           >
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input type="number" placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
