351f059a5101936f58b8c213ab8d9bd37c07b3d7
andyrich75 <arichard101@gmail.com>
2020-03-02 14:48:05 -0500

    added the balance and Income

--- budgetapp/src/App.js
+++ budgetapp/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Header from '../components/Header';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpense } from './components/IncomeExpense';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpense />
+      </div>
     </div>
   );
 }
