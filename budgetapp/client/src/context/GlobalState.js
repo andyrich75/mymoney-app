0f2fd7090c26995802fc9278aa6d21b8b2824906
andyrich75 <arichard101@gmail.com>
2020-03-02 21:51:06 -0500

    added the global state for the get transactions

--- budgetapp/client/src/context/GlobalState.js
+++ budgetapp/client/src/context/GlobalState.js
@@ -1,15 +1,12 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
-
+import axios from 'axios';
 
 //create an initial state 
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Salary', amount: 3000},
-        { id: 2, text: 'Car Payment', amount: -300},
-        { id: 3, text: 'Food', amount: -100},
-        { id: 4, text: 'Check for Birthday', amount: 50},
-    ]
+    transactions: [],
+    error: null,
+    loading: true
 }
 
 //create Global context to be passed in
@@ -19,6 +16,23 @@ export const GlobalContext = createContext(initialState);
 //Provider component
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    //Action
+    async function getTransactions() {
+        try {
+            const res = await axios.get('/api/v1/transaction');
+
+            dispatch({
+                type: 'GET_TRANSACTIONS',
+                payload: res.data.data
+            });
+        } catch (err) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            });
+        }
+    }
 
     //adding the delete function
     function deleteTransaction(id) {
