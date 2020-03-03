2ed200581cc225c34e8b00ab528e0dc1c76aeff0
andyrich75 <arichard101@gmail.com>
2020-03-02 21:26:11 -0500

    Added the APIs for the DB

--- budgetapp/server.js
+++ budgetapp/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-const connectDB = require('./config/config,env')
+const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'});
 
@@ -10,6 +10,8 @@ connectDB();
 
 const transactions = require('./routes/transactions');
 const app = express();
+
+app.use(express.json());
 
 app.use('/api/v1/transactions', transactions);
 
