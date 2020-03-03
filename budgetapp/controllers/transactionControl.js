01c39a29afdd8481a2dc2bac0b9b64713774be3e
andyrich75 <arichard101@gmail.com>
2020-03-02 20:41:55 -0500

    Added the get transaction.find in transcontrol

--- budgetapp/controllers/transactionControl.js
+++ budgetapp/controllers/transactionControl.js
@@ -1,17 +1,32 @@
+const Transaction = require('../models/Transaction')
+
 //get all transactions
 // route /api/v1/transactions
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+    try {
+        const transaction = await Transaction.find();
+        
+        return res.status(200).json({
+            success: true,
+            count: this.getTransactions.length,
+            data: transactions
+        });
+    } catch (err) {
+        return res.send(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
 }
 
 //add transaction
 // route POST /api/v1/transactions
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
     res.send('POST transaction');
 }
 
-//delete transactions
+//delete transaction
 // route /api/v1/transactions/:id
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
     res.send('DELETE transaction');
 }
